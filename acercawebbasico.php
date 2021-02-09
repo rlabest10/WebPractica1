c40dfc7803759e21eb6f23edd44343ce16fbb9e6
rlabest10 <roger.arana@gmail.com>
2021-02-09 10:03:51 -0600

    Actualizar Web

--- acercawebbasico.php
+++ acercawebbasico.php
@@ -1,31 +1,26 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Diseño de Basico</title>
+    <title>Acerca Web Basico</title>
     <link rel="stylesheet" href="css/estilo.css">
-    <link rel="stylesheet" href="css/mobile.css">
 </head>
-<body>
-    <!-- Aqui inicial el NavBar -->
+<body>    <!-- Aqui inicial el NavBar -->
     <nav>
         <div class="inv-image">
             <img src="img/inventario/image12.jpg" alt="" height="60px" border:"1">
         </div>
         <ul class="nav-options">
-        <li> <a href="index.html"> Inicio </a> </li>
+            <li> <a href="index.html"> Inicio </a> </li>
             <li> <a href="#">Productos </a></li>
             <li> <a href="#">Consultas </a></li>
             <li> <a href="#">Reportes </a></li>
-            <li> <a href="acercawebbasico.php">Acerca... </a></li></ul>
+            <li> <a href="acercawebbasico.php">Acerca... </a></li>
+        </ul>
     </nav>
     <!-- Aqui Termina el NavBar -->
-    <h1>Inicio del Sistema</h1>
-        JAJAJA inicio de Prueba
-    <div class="main_section">
-    
-    </div>
 
+    <h1>Acerca del Sistema</h1>
 </body>
 </html>
